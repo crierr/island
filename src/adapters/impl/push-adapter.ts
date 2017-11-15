acd34518f58093d19f4bd5b2435511bc0180d293
SeungJin Oh <soh@ea.com>
2017-11-15 18:29:24 +0900

    Keep backward compatibility for AmqpChannelPoolAdapter

--- src/adapters/impl/push-adapter.ts
+++ src/adapters/impl/push-adapter.ts
@@ -3,23 +3,32 @@ import { AmqpChannelPoolService } from '../../services/amqp-channel-pool-service
 import PushService from '../../services/push-service';
 import { FatalError, ISLAND } from '../../utils/error';
 import ListenableAdapter from '../listenable-adapter';
+import { AmqpChannelPoolAdapter } from './amqp-channel-pool-adapter';
 
 export interface PushAdapterOptions {
   urls: string[];
   poolSize?: number;
   prefetchCount?: number;
+  amqpChannelPoolAdapter?: AmqpChannelPoolAdapter;
 }
 
 export default class PushAdapter extends ListenableAdapter<PushService, PushAdapterOptions> {
   async initialize(): Promise<void> {
     if (!this.options) throw new FatalError(ISLAND.FATAL.F0025_MISSING_ADAPTER_OPTIONS);
-    const { urls, poolSize, prefetchCount } = this.options;
-    const channelPools = await Promise.all(map(urls, async url => {
-      const pool = new AmqpChannelPoolService();
-      await pool.initialize({ url, poolSize, prefetchCount });
-      await pool.waitForInit();
-      return pool;
-    }));
+    const { urls, poolSize, prefetchCount, amqpChannelPoolAdapter } = this.options;
+    let channelPools: AmqpChannelPoolService[];
+    if (!amqpChannelPoolAdapter) {
+      channelPools = await Promise.all(map(urls, async url => {
+        const pool = new AmqpChannelPoolService();
+        await pool.initialize({ url, poolSize, prefetchCount });
+        await pool.waitForInit();
+        return pool;
+      }));
+    } else {
+      const channelPoolService = amqpChannelPoolAdapter.adaptee;
+      await channelPoolService.waitForInit();
+      channelPools = [channelPoolService];
+    }
     this._adaptee = new PushService();
     return this._adaptee.initialize(channelPools);
   }
